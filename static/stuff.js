a64cc43e0ddbbbad20c30837839745e4930ec78a
CalSch <cal.schwick@gmail.com>
2025-06-22 15:41:20 -0500

    added analytics

---
+++ static/stuff.js
@@ -0,0 +1,28 @@
+console.log("yo")
+let isNew=false;
+let id=localStorage.getItem("session");
+console.log(`id=${id}`);
+if (id==null) {
+  isNew=true;
+  let newId = ( typeof self.crypto !== "undefined" && typeof self.crypto.randomUUID !== "undefined" ? "uuid:"+self.crypto.randomUUID() : "rand:"+Math.floor(Math.random()*10000000).toString());
+  console.log(newId);
+  localStorage.setItem("session",newId);
+  console.log("set session token");
+  id=newId;
+}
+
+let count=0;
+let start=Date.now();
+while (Date.now()-start<100)
+  count++;
+console.log(count);
+
+fetch(`https://home.calschwick.net/analytics/submit?type=calvin-website&url=${encodeURIComponent(window.location)}&token=${encodeURIComponent(id)}&new=${isNew?1:0}&count=${count}`, {
+
+  method: "POST",
+
+}).then(res => {
+
+  console.log("Request complete! response:", res);
+
+});
